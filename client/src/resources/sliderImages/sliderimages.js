8320f5253b464c8c80391894b96c1fc5badd6f6a
Venkata Raghu <gollapudi.v@northeastern.edu>
2023-07-20 17:21:19 -0400

    Added components for the landing page

---
+++ client/src/resources/sliderImages/sliderimages.js
@@ -0,0 +1,20 @@
+import SliderImage_1 from "../sliderImages/SliderImage_1.png"
+import SliderImage_2 from "../sliderImages/SliderImage_2.png"
+import SliderImage_3 from "../sliderImages/SliderImage_3.png"
+
+
+export const sliderImages= [
+    {
+        id: 1,
+        img: SliderImage_1,
+    }
+    ,
+    {
+        id: 2,
+        img: SliderImage_2,
+    },
+    {
+        id: 3,
+        img: SliderImage_3,
+    }
+]
