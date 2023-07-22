a5e96788ff17be8ca289ed2b55081518523aa3f6
Venkata Raghu <gollapudi.v@northeastern.edu>
2023-07-22 18:50:13 -0400

    backed code changed for build

--- api/index.js
+++ api/index.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
