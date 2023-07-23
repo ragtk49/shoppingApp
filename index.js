751a525d723653f4b670c2e9075b54cedaca5aa2
Venkata Raghu <gollapudi.v@northeastern.edu>
2023-07-22 21:14:26 -0400

    decoupled frontend from repo

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
 const stripeRoute = require('./routes/stripe');
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -30,7 +29,7 @@ app.use('/api/cart', cartRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
